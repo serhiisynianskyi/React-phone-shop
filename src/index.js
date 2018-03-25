c4e770e20e5c37dd952fd85326364427fb287b0a
Sergius <kfzadrser@gmail.com>
2018-03-26 00:10:36 +0300

    update(index): change layout, import dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {syncHistoryWithStore} from 'react-router-redux'
+import {Router, Route, browserHistory} from 'react-router'
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from 'reducers'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
+
+import './main.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(reducers, composeWithDevTools(
+	applyMiddleware(thunk)
+));
+
+const history = syncHistoryWithStore(browserHistory, store);
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route component={Layout}>
+				<Route path='/' component={Phones} />
+			</Route>
+		</Router>
+	</Provider>, document.getElementById('root'));
 registerServiceWorker();
